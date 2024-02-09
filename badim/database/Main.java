46a6223eea36da4206db349816a39a0a4e34a59e
lololowek <lololowek00@mail.ru>
2024-02-09 09:01:00 +0300

    FINISHED

--- badim/database/Main.java
+++ badim/database/Main.java
@@ -54,9 +54,35 @@ public class Main {
                                 }
                             } catch (SQLException e) {throw new RuntimeException(e);}}
                         coconut = false;}
-                    else {System.out.print("Неверные данные либо ваш аккаунт не существует\n");}
+                    else {System.out.print("Неверные данные либо ваш аккаунт не существует");}
                 case 2:
-
+                    String dfasdsda = scanner.nextLine();
+                    System.out.println("Логин: ");
+                    String Login = scanner.nextLine();
+                    System.out.println("Пароль: ");
+                    String Password = scanner.nextLine();
+                    boolean isUserExist = false;
+                    try (PreparedStatement ps = dbc.getConnection().prepareStatement("select 1 from data where login = ? and password=?")) {
+                        ps.setString(1, Login);
+                        ps.setString(2, Password);
+                        try (ResultSet rs = ps.executeQuery()) {
+                            if (rs.next()) {
+                                isUserExist = true;
+                            }
+                        }
+                    } catch (SQLException e) {throw new RuntimeException(e);}
+                    if (isUserExist) {
+                            System.out.print("Аккаунт с таким логином уже существует");
+                            coconut = false;
+                        }
+                        else {
+                            String query = "INSERT INTO data (login, password) VALUES (?, ?)";
+                            try (PreparedStatement preparedStatement = dbc.getConnection().prepareStatement(query)) {
+                                preparedStatement.setString(1, Login);
+                                preparedStatement.setString(2, Password);
+                                int affectedRows = preparedStatement.executeUpdate();
+                            System.out.println("Аккаунт создан.");}
+                    catch (SQLException e) {e.printStackTrace();}}
             }
         }
     }
